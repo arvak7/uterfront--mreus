c6c6de1d764de0e0355c6bed5d6526d4542de79c
mgarcia <mgarcia@hiberus.com>
2020-04-19 13:27:46 +0200

    MREUS-UTER | CREATE empty components of vehicles and vehicle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppComponent } from './app.component';
 import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 import { VehicleComponent } from './pages/vehicle/vehicle.component';
 import { from } from 'rxjs';
+import { DriversComponent } from './pages/drivers/drivers.component';
+import { DriverComponent } from './pages/driver/driver.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VehiclesComponent,
-    VehicleComponent
+    VehicleComponent,
+    DriversComponent,
+    DriverComponent
   ],
   imports: [
     BrowserModule,
